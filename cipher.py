1cb4daa511def2b6e2d0d3e0a3ffb359431f1037
jannand09 <138733341+jannand09@users.noreply.github.com>
2024-11-24 00:16:48 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -8,15 +8,84 @@ Created on Wed Nov 20 21:11:27 2024
 text = 'Hello Zaira'
 shift = 3
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
 
-for char in text.lower():
-    if char == ' ':
-        encrypted_text += char
-    else:
-        index = alphabet.find(char)
-        new_index = (index + shift) % len(alphabet)
-        encrypted_text += alphabet[new_index]
-print('plain text:', text)
-print('encrypted text:', encrypted_text)
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+
+caesar(text, shift)
+
+
+# custom_key = 'python'
+
+# def vigenere(message, key):
+#     key_index = 0
+#     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#     encrypted_text = ''
+
+#     for char in message.lower():
+    
+#         # Append space to the message
+#         if char == ' ':
+#             encrypted_text += char
+#         else:        
+#             # Find the right key character to encode
+#             key_char = key[key_index % len(key)]
+#             key_index += 1
+
+#             # Define the offset and the encrypted letter
+#             offset = alphabet.index(key_char)
+#             index = alphabet.find(char)
+#             new_index = (index + offset) % len(alphabet)
+#             encrypted_text += alphabet[new_index]
+    
+#     return encrypted_text
+    
+# encryption = vigenere(text, custom_key)
+
+
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'happycoding'
+
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
